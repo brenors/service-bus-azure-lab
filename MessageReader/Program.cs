f7d018052582ddd8db15f5800a758014878aa7d2
Breno Sedrez <101011280+brenors@users.noreply.github.com>
2023-08-01 10:13:55 -0300

    mudança nos comentários

--- MessageReader/Program.cs
+++ MessageReader/Program.cs
@@ -5,24 +5,19 @@
  {
      class Program
      {   
-         /* The `<serviceBus-connection-string>` placeholder represents
-            the connection string to the target Azure Service Bus namespace */
-         static string serviceBusConnectionString = "<string connection with azure service bus>";
+         static string connectionStringServiceBus = "<string de conexão do Service Bus na Azure >";
             
-         /* To create a string constant named "queueName" with a value
-            of "messagequeue", matching the name of the Service Bus queue.*/
-         static string queueName = "messagequeue";
+         static string filaNome = "<nome da fila criada no Service Bus na Azure>";
          static ServiceBusClient client= default;
 
-         /* Create a ServiceBusProcessor that will be used to process messages from the queue */
+         /* Inicializa um ServiceBusProcessor que será usado para processar as mensagens da fila */
          static ServiceBusProcessor processor = default;
 
 
          static async Task MessageHandler(ProcessMessageEventArgs args)
          {   
-             /* To create a static async "MessageHandler" task that displays 
-                the body of messages in the queue as they are being processed 
-                and deletes them after the processing completes */
+             /* Cria um static async "MessageHandler" task que cria o corpo das mensagens na fila
+              ao passo em que são processadas e remove elas após cada processamento completo */
 
              string body = args.Message.Body.ToString();
              Console.WriteLine($"Received: {body}");
@@ -30,39 +25,37 @@
          }
          static Task ErrorHandler(ProcessErrorEventArgs args)
          {   
-             /* To create a static async "ErrorHandler" task that manages 
-                any exceptions encountered during message processing */
+             /* Cria um static async "ErrorHandler" task que gerencia qualquer
+              exceção encontrada durante o processamento de mensagem */
              Console.WriteLine(args.Exception.ToString());
              return Task.CompletedTask;
          }
 
          static async Task Main()
          {   
-             /* To initialize "client" of type "ServiceBusClient" that will provide 
-                connectivity to the Service Bus namespace and "processor" that will
-                be responsible for processing of messages */
-             client = new ServiceBusClient(serviceBusConnectionString);
-             processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+             /* Inicializa "client" do tipo "ServiceBusClient" que vai prover conectividade
+              no diretório do Service Bus e "processor" que será responsável por processar as mensagens */
+             client = new ServiceBusClient(connectionStringServiceBus);
+             processor = client.CreateProcessor(filaNome, new ServiceBusProcessorOptions());
              try
              {   
-                 /* To create a try block, which first implements a message and error
-                     processing handler, initiates message processing, and stops
-                     processing following a user input */
+                 /* Bloco try que, primeiramente implementa um handler de mensagem e erro,
+                 inicia o processamento da mensagem, e para o processamento seguindo o input do usuário */
                  processor.ProcessMessageAsync += MessageHandler;
                  processor.ProcessErrorAsync += ErrorHandler;
 
                  await processor.StartProcessingAsync();
-                 Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                 Console.WriteLine("Ouvindo mensagens. Aperque qualquer tecla para parar o serviço.");
                  Console.ReadKey();
 
-                 Console.WriteLine("\nStopping the receiver...");
+                 Console.WriteLine("\nParando serviço...");
                  await processor.StopProcessingAsync();
-                 Console.WriteLine("Stopped receiving messages");
+                 Console.WriteLine("Serviço parado.");
              }
              finally
              {
-                /* To create a finally block that asynchronously disposes of the "processor"
-                    and "client" objects, releasing any network and unmanaged resources */
+                /* Cria um bloco finally que, de maneira assíncrona, da um dispose nos objetos
+                 "processor" e "client", aliviando qualquer recurso nao gerenciado de rede */
                  await processor.DisposeAsync();
                  await client.DisposeAsync();
              }
