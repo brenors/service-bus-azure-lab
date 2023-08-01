192a51309b3c49f8143b050037827bf9686c1e55
Breno Sedrez <101011280+brenors@users.noreply.github.com>
2023-08-01 10:13:45 -0300

    mudança dos comentários para português para ficar melhor entendido ao realizar a leitura

--- MessagePublisher/Program.cs
+++ MessagePublisher/Program.cs
@@ -5,17 +5,16 @@
  {
      public class Program
      {
-         private const string connectionStringServiceBus = "<string connection with azure service bus>";
+         private const string connectionStringServiceBus = "<string connection do recurso na Azure>";
 
-         private const string nomeFila = "messagequeue";
+         private const string nomeFila = "<nome da sua fila criada no recurso da Azure>";
 
          private const int numMensagens = 100;
 
-         /* To create a Service Bus client that will own the connection to the target queue */
+         /* Inicializa um Service Bus client que será dono da conexão da fila alvo */
          static ServiceBusClient client = default!;
 
-         /* To create a Service Bus sender that will be 
-            used to publish messages to the target queue */
+         /* Inicializa um Service Bus sender que será usado para enviar(publicar) as mensagens na fila alvo */
          static ServiceBusSender sender = default!;
 
          public static async Task Main(string[] args)
@@ -30,8 +29,7 @@
               de mensagens dentro do batch usando o método "TryAddMessage" */
              using ServiceBusMessageBatch messagemBatch = await sender.CreateMessageBatchAsync();
 
-             /* To add messages to a batch and throw an exception if a message
-                size exceeds the limits supported by the batch */
+             /* Adiciona uma mensagem no batch e da um throw em uma exceção caso ultrapasse o limite do batch suportado */
              for (int i = 1; i <= numMensagens; i++)
              {
                  if (!messagemBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
@@ -41,15 +39,14 @@
              }
              try
              {
-                 /* To create a try block, with "sender" asynchronously 
-                    publishing messages in the batch to the target queue */
+                /* Try com o "sender" enviando as mensagens de maneira assíncrona no batch enfileirando no alvo */
                  await sender.SendMessagesAsync(messagemBatch);
                  Console.WriteLine($"A batch of {numMensagens} messages has been published to the queue.");
              }
              finally
              {
-                 /* To create a finally block that asynchronously disposes of the "sender"
-                    and "client" objects, releasing any network and unmanaged resources */
+                 /* Finally que da um dispose assíncrono no "sender e no "client", aliviando 
+                 qualquer recurso nao gerenciado de rede */
                  await sender.DisposeAsync();
                  await client.DisposeAsync();
              }
